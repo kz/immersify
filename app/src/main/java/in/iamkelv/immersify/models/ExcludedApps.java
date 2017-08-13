a70920da7c40f05df206f0ee1f20b57ebea28c47
Kelvin Zhang <me@iamkelv.in>
2017-08-13 21:45:24 +0100

    Add ExcludedApps class

---
+++ app/src/main/java/in/iamkelv/immersify/models/ExcludedApps.java
@@ -0,0 +1,32 @@
+package in.iamkelv.immersify.models;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Set;
+
+public class ExcludedApps {
+    private static final String PREFS_NAME = "ExcludedAppsFile";
+
+    private SharedPreferences settings;
+
+    public ExcludedApps(Context context) {
+        settings = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+    }
+
+    public Set<String> getAll() {
+        return settings.getAll().keySet();
+    }
+
+    public void add(String packageName) {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putBoolean(packageName, true);
+        editor.apply();
+    }
+
+    public void remove(String packageName) {
+        SharedPreferences.Editor editor = settings.edit();
+        editor.remove(packageName);
+        editor.apply();
+    }
+}
